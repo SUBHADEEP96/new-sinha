daffd3cfcda2ebe06ff52f23789d2bb18769b838
raja <thinksurfmedia@gmail.com>
2020-02-14 12:32:34 +0530

    new-sinha

---
+++ index.php
@@ -0,0 +1,249 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--custo css-->
+	<link rel="stylesheet" href="style.css">
+	   
+ <!-- Favicons -->
+  <link href="image/favi.jpg" rel="icon">
+<!--font awesome-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+	<!--google font-->
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
+	
+    <title>NEW SINHA JEWELLERS</title>
+	<style>
+	body{
+
+font-family: 'Playfair Display', serif;
+
+	
+}
+.dropdown:hover>.dropdown-menu{
+		
+		display:block;
+		
+	}
+@media only screen and (max-width: 600px) {
+.firsth1{
+    font-size:14px !important;
+  }
+}
+
+
+#img img{
+	
+	
+		filter:grayscale(150%);
+		transition:1s;
+}
+#img img:hover{
+	filter:grayscale(0);
+		
+	
+}
+
+</style>
+  </head>
+  <body>
+   <div class="top-bar bg-dark " id="top-bar" style="background:linear-gradient(to right, #FBCAB9, #fff);">
+	
+        <div class="container">
+            <div class="row align-items-center">
+			
+                <div class="col-lg-6 col-md-6">
+                    <div class="top-bar-left" style="color:#CD181F">
+                        <i class="fa fa-phone-square" aria-hidden="true"></i>
+                        <span class="ml-2"><a href="tel:089817 19199" style="color:#CD181F">0123456789</a></span>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 ml-lg-auto col-md-6">
+                    <ul class="d-flex list-unstyled header-socials float-lg-right">
+                        <li><a href="#" style="color:#CD181F" > <i class="fa fa-facebook-square" aria-hidden="true"></i></li>
+                         &nbsp;&nbsp;<li><a href="#"  style="color:#CD181F" > <i class="fa fa-envelope" aria-hidden="true"></i></li>
+                        &nbsp; &nbsp; <li><a href="#"  style="color:#CD181F" ><i class="fa fa-google" aria-hidden="true"></i></li>
+                        
+                      </ul> 
+                </div>
+				
+            </div>
+        </div>
+		
+    </div>
+  <!----->
+  <div class="head" style="background:linear-gradient(to right, #fff, #FBCAB9);">
+  
+<nav class="navbar navbar-expand-lg navbar-light " >
+  <a class="navbar-brand" href="index.php"><img src="image/newsinhajewellers.jpg" style="height:60px;box-shadow: 10px 10px rgba(255,255,255,0.3);"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+   
+  <div class="collapse navbar-collapse" id="navbarNav">
+
+    <ul class="navbar-nav" >
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:#CD181F !important"><i class="fa-2x fa fa-home" aria-hidden="true"></i></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color:#CD181F !important" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <b>Our Collections</b>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background:linear-gradient(to right, #fff, #FBCAB9);">
+		<div class="row" style="width:580px;">
+		<div class="col-lg-4" >
+          <a class="dropdown-item text-danger" href="#">Necklaces</a>
+          <a class="dropdown-item text-danger" href="#">Chains</a>
+          <a class="dropdown-item text-danger" href="#">Pendants</a>
+		</div>
+		
+		<div class="col-lg-4">
+          <a class="dropdown-item text-danger" href="#">Earrings</a>
+          <a class="dropdown-item text-danger" href="#">Maangtika</a>
+          <a class="dropdown-item text-danger" href="#">Anklets</a>
+		  
+		</div>
+		
+		<div class="col-lg-4">
+          <a class="dropdown-item text-danger" href="#">Bangles</a>
+          <a class="dropdown-item text-danger" href="#">Bracelets</a>
+          <a class="dropdown-item text-danger" href="#">Rings</a>
+		  
+		</div>
+        
+		
+		
+		</div>
+		</div>
+      </li>
+    </ul>
+	
+	 <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:#CD181F !important"><b>New Sinha Story</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:#CD181F !important"><b>Contact Us</b></a>
+      </li>
+     
+     
+    </ul>
+	
+  </div>
+ 
+</nav>
+</div>
+<!---->
+<div style="background: linear-gradient(to right, #fbd786, #f7797d);">
+<div class="container">
+<div id="newsinhajewellers" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#newsinhajewellers" data-slide-to="0" class="active" style=" width: 12px;
+    height: 12px; border-radius: 100%; background-color:#FBD46D;"></li>
+    <li data-target="#newsinhajewellers" data-slide-to="1" style=" width: 12px;
+    height: 12px; border-radius: 100%; background-color: #FBD46D;"></li>
+    <li data-target="#newsinhajewellers" data-slide-to="2" style=" width: 12px;
+    height: 12px; border-radius: 100%; background-color:#FBD46D;"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/sn11.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/sn11.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/sn11.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#newsinhajewellers" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#newsinhajewellers" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</div><br>
+<!----->
+<div class="container">
+<div class="firsth1 mb-3">
+<h1 class="text-center text-muted" style="font-size:25px">ONE STOP DESTINATION TO BUY GOLD & DIAMOND JEWELLERY</h2>
+</div>
+<div class="row">
+
+<div class="col-lg-6 mb-3 ">
+<a href="#"><img src="image/sinha1.jpg" style="max-width:100%;height:auto;"></a>
+</div>
+
+<div class="col-lg-6 mb-3">
+
+<a href="#"><img src="image/sinha2.jpg" style="max-width:100%;height:auto;"></a>
+
+</div>
+
+
+</div><br>
+<div class="container">
+<div class=" mb-3">
+<h1 class="text-center text-muted" style="font-size:25px">OUR DESIGN</h2>
+</div>
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+  <div class="row">
+    <div class="col-md-8 mb-3" id="img"><a href="#"><img src="image/sn11.jpg" style="max-width:100%;height:auto;"></a></div>
+    <div class="col-6 col-md-4 mb-3" id="img"><a href="#"><img src="image/sn3.jpg" style="max-width:100%;height:auto;"></a></div>
+  </div>
+
+  <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+  <div class="row">
+    <div class=" col-md-4 mb-3" id="img"><a href="#"><img src="image/sn5.jpg" style="max-width:100%;height:auto;"></a></div>
+    <div class=" col-md-4 mb-3" id="img"><a href="#"><img src="image/sn4.jpg" style="max-width:100%;height:auto;"></a></div>
+    <div class=" col-md-4 mb-3 " id="img"><a href="#"><img src="image/sn7.jpg" style="max-width:100%;height:auto;"></a></div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-md-4 mb-3" id="img"><a href="#"><img src="image/sn10.jpg" style="max-width:100%;height:auto;"></a></div>
+    <div class="col-md-4 mb-3" id="img"><a href="#"><img src="image/sn6.jpg" style="max-width:100%;height:auto;"></a></div>
+	<div class="col-md-4 mb-3" id="img"><a href="#"><img src="image/sn8.jpg" style="max-width:100%;height:auto;"></a></div>
+	
+  </div>
+</div>
+<br>
+<!------>
+<div class="container-fluid text-danger" style="background:linear-gradient(to right, #fff, #FBCAB9);" ><br>
+<div class="row">
+<div class="col-lg-6" align="center">
+<p style="font-size:14px">Copyright &copy; 2020 <a href="index.php" class="text-danger">NEW SINHA JEWELLERS</a>. All rights reserved</p>
+</div>
+<div class="col-lg-6 col-sm-12 d-inline" align="center" style="font-size:14px">
+<a href="index.php" class="text-danger">Home |</a>
+
+
+<a href="contact.php" class="text-danger">Contact |</a>
+
+<a href="privacy-policy.php" class="text-danger">Privacy Policy |</a>
+<a href="terms-of-use.php" class="text-danger"> Terms of Use</a>
+
+</div>
+
+</div><br>
+</div>
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
